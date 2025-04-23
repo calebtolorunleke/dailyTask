6225645c9e3e17d3e7f2fdff58dff4fe267dae52
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-04-23 20:12:05 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+function addTask() {
+    const newTasked = document.getElementById('newTask').value
+    const newTaskedzero = document.getElementById('newTask')
+
+    console.log(newTasked)
+
+    // const taskInput = getElementById('newTask')
+    const newTask = document.getElementById('newTask').value.trim()
+
+
+    if (newTask !== '') {
+        const taskContainer = document.getElementById('taskContainer')
+        const taskElement = document.createElement('li')
+        taskElement.textContent = newTask
+        taskContainer.appendChild(taskElement)
+
+        newTaskedzero.value = ''
+    } else {
+        alert('Please enter a task')
+    }
+}
